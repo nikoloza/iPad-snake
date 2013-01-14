2cdfdde0e3ca478f115eb52d839d55e5b7c7e41c
Nikoloz Tsimakuridze <n.tsimakuridze@freeuni.edu.ge>
2013-01-14 17:02:09 +0400

    Changed svg to html (divs)

---
+++ www/js/index.js
@@ -0,0 +1,20 @@
+const snake_colours = ['#3333ff','#ffff33'];
+
+function createRect(c_x,c_y,col)
+{
+    $('#gameBoard').append('<div class=\'boardCell\' style=\'background-color: ' + col + '\'></div>');
+}
+
+var app = {
+    // Application Constructor
+    initialize: function(W,H) {
+        this.boardWidth = W;
+        this.boardHeight = H;
+
+        // setup game board
+        var cc_x,cc_y;
+        for (cc_y = 0; cc_y < this.boardHeight; cc_y++)
+            for (cc_x = 0; cc_x < W; cc_x++)
+              createRect(cc_x,cc_y,snake_colours[(cc_x+cc_y)%2]);
+    },
+};
